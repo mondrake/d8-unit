7d6aa3d9d066f2de94ebc7c6e6c48679a2b17366
mondrake <mondrake.org@gmail.com>
2024-05-02 21:24:39 +0200

    Create rector.php

---
+++ rector.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\PHPUnit\AnnotationsToAttributes\Rector\ClassMethod\DataProviderAnnotationToAttributeRector;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
+use Rector\PHPUnit\AnnotationsToAttributes\Rector\ClassMethod\TestWithAnnotationToAttributeRector;
+use Rector\PHPUnit\AnnotationsToAttributes\Rector\Class_\AnnotationWithValueToAttributeRector;
+use Rector\Php52\Rector\Property\VarToPublicPropertyRector;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/composer',
+//        __DIR__ . '/core/tests',
+//        __DIR__ . '/core/modules/system',
+        __DIR__ . '/core',
+    ])
+//    ->withSkipPath(
+//        __DIR__ . '/core/tests/Drupal/Tests/Component/Annotation/Doctrine/Fixtures',
+//    )
+//    ->withSkipPath(
+//        __DIR__ . '/core/modules/**/src',
+//    )
+    // uncomment to reach your current PHP version
+    // ->withPhpSets()
+    ->withRules([
+//        AddVoidReturnTypeWhereNoReturnRector::class,
+//        DataProviderAnnotationToAttributeRector::class,
+//        TestWithAnnotationToAttributeRector::class,
+        VarToPublicPropertyRector::class,
+//        Rector\PHPUnit\AnnotationsToAttributes\Rector\Class_\AnnotationWithValueToAttributeRector
+    ])
+    ->withImportNames(
+        importDocBlockNames: false,
+        importShortClasses: false,
+        removeUnusedImports: false,
+    );
