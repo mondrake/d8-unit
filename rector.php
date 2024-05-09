71ce4271611b95a75bfe9ff2fbcccec5587765dc
mondrake <mondrake.org@gmail.com>
2024-05-09 21:41:35 +0200

    Update rector.php

--- rector.php
+++ rector.php
@@ -2,111 +2,22 @@
 
 declare(strict_types=1);
 
-use PhpParser\Node;
-use PhpParser\Node\AttributeGroup;
-use PhpParser\Node\Stmt\Class_;
-use PHPStan\Reflection\ClassReflection;
-use PHPStan\Reflection\ReflectionProvider;
-use Rector\Reflection\ReflectionResolver;
-use Rector\Php80\NodeAnalyzer\PhpAttributeAnalyzer;
-use Rector\PhpAttribute\NodeFactory\PhpAttributeGroupFactory;
-use Rector\PHPUnit\NodeAnalyzer\TestsNodeAnalyzer;
-use Rector\Rector\AbstractRector;
-use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
-use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
-
-use Drupal\KernelTests\KernelTestBase;
-use Drupal\Tests\BrowserTestBase;
 use Rector\Config\RectorConfig;
-
-final class FillRunTestInIsolationRector extends AbstractRector
-{
-    public function __construct(
-        private readonly ReflectionProvider $reflectionProvider,
-        private readonly PhpAttributeGroupFactory $phpAttributeGroupFactory,
-        private readonly PhpAttributeAnalyzer $phpAttributeAnalyzer,
-        private readonly TestsNodeAnalyzer $testsNodeAnalyzer,
-        private readonly ReflectionResolver $reflectionResolver,
-    ) {
-    }
-
-    public function getRuleDefinition(): RuleDefinition
-    {
-        return new RuleDefinition('Adds `#[RunTestsInSeparateProcesses]` attribute to Kernel and Functional tests.', []);
-    }
-
-    /**
-     * @return array<class-string<Node>>
-     */
-    public function getNodeTypes(): array
-    {
-        return [Class_::class];
-    }
-
-    /**
-     * @param Class_ $node
-     */
-    public function refactor(Node $node): ?Node
-    {
-        $className = $this->getName($node);
-
-        if ($className === null) {
-            return null;
-        }
-
-        $classReflection = $this->reflectionResolver->resolveClassReflection($node);
-        if (! $classReflection instanceof ClassReflection) {
-            return null;
-        }
-
-        if (! $classReflection->isClass()) {
-            return null;
-        }
-
-        if (! $classReflection->isSubclassOf(KernelTestBase::class) && ! $classReflection->isSubclassOf(BrowserTestBase::class)) {
-            return null;
-        }
-
-        if (\str_starts_with($className, 'AnonymousClass')) {
-            return null;
-        }
-
-        if ($this->phpAttributeAnalyzer->hasPhpAttributes($node, [
-            'PHPUnit\\Framework\\Attributes\\RunTestsInSeparateProcesses',
-        ])) {
-            return null;
-        }
-
-        $coversAttributeGroup = $this->createAttributeGroup();
-
-        $node->attrGroups = array_merge($node->attrGroups, [$coversAttributeGroup]);
-
-        return $node;
-    }
-
-    private function createAttributeGroup(): AttributeGroup
-    {
-        $attributeClass = 'PHPUnit\\Framework\\Attributes\\RunTestsInSeparateProcesses';
-
-        return $this->phpAttributeGroupFactory->createFromClassWithItems($attributeClass, []);
-    }
-}
+use Rector\PHPUnit\AnnotationsToAttributes\Rector\Class_\CoversAnnotationWithValueToAttributeRector;
+use Rector\PHPUnit\AnnotationsToAttributes\Rector\ClassMethod\DataProviderAnnotationToAttributeRector;
 
 return RectorConfig::configure()
     ->withPaths([
-        __DIR__ . '/composer',
-        __DIR__ . '/core',
+        __DIR__ . '/core/tests/Drupal/Tests/Component',
     ])
     ->withSkip([
-        '/core/tests/Drupal/Tests/Component/Annotation/Doctrine/Fixtures/*',
-        '/core/tests/fixtures/*',
-        '/core/tests/PHPStan/*',
-        // Below to avoid unwanted conversions of FQCNs to short syntax.       
+        __DIR__ . '/core/tests/Drupal/Tests/Component/Annotation/Doctrine/Fixtures',
         '*/ProxyClass/*',
         '*.api.php',
     ])
     ->withRules([
-        FillRunTestInIsolationRector::class,
+        CoversAnnotationWithValueToAttributeRector::class,
+        DataProviderAnnotationToAttributeRector::class,
     ])
     ->withImportNames(
         importDocBlockNames: false,
