eeb44f9034e066a197a5a0c9913620dc82193c1a
mondrake <mondrake.org@gmail.com>
2024-05-10 16:23:09 +0200

    Update rector.php

--- rector.php
+++ rector.php
@@ -2,9 +2,126 @@
 
 declare(strict_types=1);
 
-use Rector\Config\RectorConfig;
-use Rector\PHPUnit\AnnotationsToAttributes\Rector\Class_\CoversAnnotationWithValueToAttributeRector;
-use Rector\PHPUnit\AnnotationsToAttributes\Rector\ClassMethod\DataProviderAnnotationToAttributeRector;
+use PhpParser\Node;
+use PhpParser\Node\Stmt\Class_;
+use PhpParser\Node\Stmt\ClassMethod;
+use PHPStan\PhpDocParser\Ast\PhpDoc\GenericTagValueNode;
+use PHPStan\PhpDocParser\Ast\PhpDoc\PhpDocTagNode;
+use Rector\BetterPhpDocParser\PhpDocInfo\PhpDocInfo;
+use Rector\BetterPhpDocParser\PhpDocInfo\PhpDocInfoFactory;
+use Rector\BetterPhpDocParser\PhpDocManipulator\PhpDocTagRemover;
+use Rector\Comments\NodeDocBlock\DocBlockUpdater;
+use Rector\Contract\Rector\ConfigurableRectorInterface;
+use Rector\PhpAttribute\NodeFactory\PhpAttributeGroupFactory;
+use Rector\PHPUnit\ValueObject\AnnotationWithValueToAttribute;
+use Rector\Rector\AbstractRector;
+use Rector\ValueObject\PhpVersionFeature;
+use Rector\VersionBonding\Contract\MinPhpVersionInterface;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\ConfiguredCodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+use Webmozart\Assert\Assert;
+
+final class DrupalAnnotationToAttributeRector extends AbstractRector implements ConfigurableRectorInterface, MinPhpVersionInterface
+{
+    /**
+     * @var AnnotationWithValueToAttribute[]
+     */
+    private array $annotationWithValueToAttributes = [];
+
+    public function __construct(
+        private readonly PhpDocTagRemover $phpDocTagRemover,
+        private readonly PhpAttributeGroupFactory $phpAttributeGroupFactory,
+        private readonly DocBlockUpdater $docBlockUpdater,
+        private readonly PhpDocInfoFactory $phpDocInfoFactory,
+    ) {
+    }
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition('Change annotations to attributes in Drupal test codebase', []);
+    }
+
+    public function getNodeTypes(): array
+    {
+        return [Class_::class, ClassMethod::class];
+    }
+
+    public function provideMinPhpVersion(): int
+    {
+        return PhpVersionFeature::ATTRIBUTES;
+    }
+
+    /**
+     * @param Class_|ClassMethod $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        $phpDocInfo = $this->phpDocInfoFactory->createFromNode($node);
+        if (! $phpDocInfo instanceof PhpDocInfo) {
+            return null;
+        }
+dump($phpDocInfo); return null;
+
+        $hasChanged = false;
+
+        foreach ($this->annotationWithValueToAttributes as $annotationWithValueToAttribute) {
+            /** @var PhpDocTagNode[] $desiredTagValueNodes */
+            $desiredTagValueNodes = $phpDocInfo->getTagsByName($annotationWithValueToAttribute->getAnnotationName());
+
+            foreach ($desiredTagValueNodes as $desiredTagValueNode) {
+                if (! $desiredTagValueNode->value instanceof GenericTagValueNode) {
+                    continue;
+                }
+
+                $attributeValue = $this->resolveAttributeValue(
+                    $desiredTagValueNode->value,
+                    $annotationWithValueToAttribute
+                );
+
+                $attributeGroup = $this->phpAttributeGroupFactory->createFromClassWithItems(
+                    $annotationWithValueToAttribute->getAttributeClass(),
+                    [$attributeValue]
+                );
+
+                $node->attrGroups[] = $attributeGroup;
+
+                // cleanup
+                $this->phpDocTagRemover->removeTagValueFromNode($phpDocInfo, $desiredTagValueNode);
+                $hasChanged = true;
+            }
+        }
+
+        if ($hasChanged) {
+            $this->docBlockUpdater->updateRefactoredNodeWithPhpDocInfo($node);
+            return $node;
+        }
+
+        return null;
+    }
+
+    /**
+     * @param mixed[] $configuration
+     */
+    public function configure(array $configuration): void
+    {
+        Assert::allIsInstanceOf($configuration, AnnotationWithValueToAttribute::class);
+        $this->annotationWithValueToAttributes = $configuration;
+    }
+
+    private function resolveAttributeValue(
+        GenericTagValueNode $genericTagValueNode,
+        AnnotationWithValueToAttribute $annotationWithValueToAttribute
+    ): mixed {
+        $valueMap = $annotationWithValueToAttribute->getValueMap();
+        if ($valueMap === []) {
+            // no map? convert value as it is
+            return $genericTagValueNode->value;
+        }
+
+        $originalValue = strtolower($genericTagValueNode->value);
+        return $valueMap[$originalValue];
+    }
+}
 
 return RectorConfig::configure()
     ->withPaths([
