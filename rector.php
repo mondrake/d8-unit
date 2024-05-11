6ceda84f67240e97edd8e8e8c005daa5c952efdb
mondrake <mondrake.org@gmail.com>
2024-05-11 13:00:51 +0200

    Update rector.php

--- rector.php
+++ rector.php
@@ -185,7 +185,7 @@ return RectorConfig::configure()
     ->withRules([
         DrupalAnnotationToAttributeRector::class,
     ])
-    ->withoutParallel()
+#    ->withoutParallel()
     ->withImportNames(
         importDocBlockNames: false,
         importShortClasses: false,
